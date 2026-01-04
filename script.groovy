cad085715d7ab0b65e0ccfc76e9454a7f864a266
isaac247 <mammanisaac01@gmail.com>
2026-01-04 22:31:52 +0100

    updated version number

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t izik247/java-app:jma-2.0 .'
+        sh 'docker build -t izik247/java-app:jma-2.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push izik247/java-app:jma-2.0'
+        sh 'docker push izik247/java-app:jma-2.1'
     }
 }
 
