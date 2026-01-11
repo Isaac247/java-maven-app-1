754e8ad2c6ccff15629d61ddccfa971dd6270b65
isaac247 <mammanisaac01@gmail.com>
2026-01-11 21:16:01 +0100

    versioning maven app

--- script.groovy
+++ script.groovy
@@ -1,3 +1,11 @@
+def incrementVersion() {
+    echo 'incrementing the application version...'
+    sh 'mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.' +
+       '${parsedVersion.minorVersion}.$((${parsedVersion.nextIncrementalVersion})) versions:commit'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "${version}-${BUILD_NUMBER}"
+}
 def buildJar() {
     echo 'building the application...'
     sh 'mvn package'
@@ -6,9 +14,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t izik247/java-app:jma-2.2 .'
+        sh "docker build -t izik247/java-app:$IMAGE_NAME ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push izik247/java-app:jma-2.2'
+        sh "docker push izik247/java-app:$IMAGE_NAME"
     }
 }
 
