72d953b2482c5ca1ac968523eee6958e13fc7684
isaac247 <mammanisaac01@gmail.com>
2026-01-12 09:14:54 +0100

    added comments to groovy script

--- script.groovy
+++ script.groovy
@@ -1,14 +1,14 @@
 def incrementVersion() {
     echo 'incrementing the application version...'
     sh 'mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.' +
-       '${parsedVersion.minorVersion}.$((${parsedVersion.nextIncrementalVersion})) versions:commit'
-    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
-    def version = matcher[0][1]
-    env.IMAGE_NAME = "${version}-${BUILD_NUMBER}"
+       '${parsedVersion.minorVersion}.$((${parsedVersion.nextIncrementalVersion})) versions:commit' //building the java maven app using incremental versioning maj:min:patch
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>' //reads the pom.xml file as an array and saves it to a matcher variable
+    def version = matcher[0][1] //[0][1] signifies the version of the app in the array
+    env.IMAGE_NAME = "${version}-${BUILD_NUMBER}" // appending the build number from jenkins with the version name saved in the IMAGE_NAME variable
 }
 def buildJar() {
     echo 'building the application...'
-    sh 'mvn package'
+    sh 'mvn clean package'
 }
 
 def buildImage() {
